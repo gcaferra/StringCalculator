fcf479e1e1d68b6790104bfdefbcb01c9445da79
Giuseppe Caferra <g.caferra@outlook.com>
2021-11-02 15:28:52 +0100

    Separator can be specified using syntax like “//[delimiter]\n[numbers…]”

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,8 +15,9 @@ namespace StringCalculator
 
         static List<string> SplitNumbers(string numbers)
         {
+            var stringParts = GetSeparatorAndString(numbers);
             var result = new List<string>();
-            var splits = numbers.Split(",");
+            var splits = stringParts.Item2.Split(stringParts.Item1);
 
             foreach (var split in splits)
             {
@@ -23,6 +25,17 @@ namespace StringCalculator
             }
 
             return result;
+
+        }
+
+        static Tuple<string,string> GetSeparatorAndString(string inputText, string defaultSeparator = ",")
+        {
+            var codeIndex = inputText.IndexOf("//", StringComparison.Ordinal);
+            if (codeIndex < 0) return new Tuple<string, string>(defaultSeparator, inputText);
+            var parts = inputText.Split("\n");
+                
+            return new Tuple<string, string>(parts[0].Replace("//", string.Empty), 
+                parts[1].Replace("\n", string.Empty));
 
         }
     }
